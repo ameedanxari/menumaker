d1ff8351308453467b612660b763b419ceafdb93
macintosh <ameed.ansari@creatrixe.com>
2025-11-16 02:26:07 -0800

    Generate iOS Project

---
+++ ios/MenuMakerTests/MenuMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMakerTests.swift
+//  MenuMakerTests
+//
+//  Created by Ameed Khalid on 16/11/2025.
+//
+
+import Testing
+@testable import MenuMaker
+
+struct MenuMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
